c9034a506a72f03c02a8e2199a6458e79ec8b6e9
HenrikEide <Henrik225@gmail.com>
2021-10-18 20:44:12 +0200

    PEP check works

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request
+from flask.templating import render_template
+from kycRequests import pep_check
 
 app = Flask(__name__)
 
 @app.route("/api/name")
 def test():
     page = request.args.get("name")
-    return f"please work {page}"
+    result = pep_check(page)
+    return result
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        return pep_check(request.args.get("name"))
--- kycRequests.py
+++ kycRequests.py
@@ -1,3 +1,8 @@
 import requests
-import mysql.connector.connection
 
+def pep_check(name):
+    hit = requests.request("GET", f"https://stacc-code-challenge-2021.azurewebsites.net/api/pep?name={name}").json()
+    if hit["numberOfHits"] == 0:
+        return "Not a PEP"
+    else:
+        return "PEP, flag for manual processing"
