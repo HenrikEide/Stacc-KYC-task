13abaa2858ba40d76a1ca709967184fc6684070b
HenrikEide <Henrik225@gmail.com>
2021-10-18 19:02:15 +0200

    First example that works

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/api/name")
 def test():
-    return "Is this even working"
+    page = request.args.get("name")
+    return f"please work {page}"
